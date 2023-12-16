cbc8d270e8d855bec49f6127dade96787bc1d3c2
Omar Abdiwali <omarabdiwali17@gmail.com>
2023-12-16 14:56:24 -0600

    added hover colour changes

--- components/noteSection.js
+++ components/noteSection.js
@@ -6,7 +6,7 @@ export default function NoteSection({ notes, title, remove, update, className })
     <div className={`flex-1 text-black bg-zinc-400 overflow-x-auto min-h-[15rem] max-h-[15rem] min-w-[20rem] w-[30rem] max-w-[30rem] ${className} rounded-lg shadow-2xl border border-b border-slate-500`}>
       <div className="flex text-lg p-3 m-3">
         <div className="flex-1">{title}</div>
-        <a href={`/notes/create/${title}`} className="text-2xl m-auto"><AiOutlinePlus /></a>
+        <a href={`/notes/create/${title}`} className="text-2xl m-auto hover:text-white"><AiOutlinePlus /></a>
       </div>
       <div className="flex flex-col mx-5">
         <div className="h-[9rem] max-h-[9rem] overflow-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-slate-700 scrollbar-track-inherit">
--- components/noteTab.js
+++ components/noteTab.js
@@ -53,8 +53,8 @@ export default function NoteTab({ note, remove, update }) {
 
   return (
     <div className="flex min-w-fit text-sm text-white bg-zinc-800 mx-5 rounded-xl mb-3 my-auto border border-b border-slate-600 h-9 max-h-9">
-      <DeleteNote disableButton={disabled} className={"ml-3 mr-2 m-auto text-lg"} func={() => remove(note)} button={<AiOutlineDelete />} noteId={note.id} />
-      <button title={!change ? "Edit title" : "Save Title"} disabled={disabled} onClick={changeType} className="mr-5 my-auto text-lg">{!change ? <AiOutlineEdit /> : <AiOutlineCheck />}</button>
+      <DeleteNote disableButton={disabled} className={"ml-3 mr-2 m-auto text-lg hover:text-gray-400"} func={() => remove(note)} button={<AiOutlineDelete />} noteId={note.id} />
+      <button title={!change ? "Edit title" : "Save Title"} disabled={disabled} onClick={changeType} className="mr-5 my-auto text-lg hover:text-gray-400">{!change ? <AiOutlineEdit /> : <AiOutlineCheck />}</button>
       <div className="pr-4 whitespace-nowrap my-auto flex-1">
         {!change ? <a href={`/notes/${note.id}`}>{title}</a> :
           <form onSubmit={saveTitle}>
@@ -62,7 +62,7 @@ export default function NoteTab({ note, remove, update }) {
           </form>
         }
       </div>
-      <ChangeSection func={update} className={"text-xl my-auto mr-3 ml-2"} button={<AiOutlineEllipsis />} noteId={note.id} important={note.importance} />
+      <ChangeSection func={update} className={"text-xl my-auto mr-3 ml-2 hover:text-gray-400"} button={<AiOutlineEllipsis />} noteId={note.id} important={note.importance} />
     </div>
   )
 }
